32a2d46ebb1b3a610dfe20128549492e079a92e4
Leo Bergqvist <leo@bergqvist.se>
2023-01-18 15:05:12 +0100

    Schedule + comments

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import asyncio
 import discord
 import openai
-import config
+import requests
+from bs4 import BeautifulSoup
 from better_profanity import profanity
+import config
 
-
+# Intents
 intents = discord.Intents.all()
 
 
@@ -40,26 +42,63 @@ class ai:
             presence_penalty=self.p_p
         )
 
+        # Return only the response
         return response['choices'][0]['text']
 
     def set_values(self, value, msg, limit, minimum, value_name):
         """Update values"""
 
+        # Check if the value is an integer
         try:
             value = int(msg.content.split()[-1])
 
+        # If not, return an error message
         except Exception as e:
             print(e, '\nValue is not a valid integer.')
 
+        # Check if the value is within the limit
         if value > limit:
             value = limit
             yield f'{value_name} limit is {limit}.'
 
+        # Check if the value is above the minimum
         if value < minimum:
             value = minimum
             yield f'{value_name} minimum is {minimum}.'
 
+        # Return the value
         yield f'{value_name} set to {value}'
+
+
+class practice:
+    """Class that handles the practice schedule."""
+
+    def __init__(self):
+        # URL to the practice schedule
+        self.url = "https://www.uppsalavolley.com/sasongstider-schemaandringar/"
+        self.current_html = requests.get(self.url, timeout=15).text
+
+    ### Get schedule ###
+
+    async def get_schedule(self):
+        """Function to get schedule"""
+
+        while True:
+            # Get the current HTML
+            soup = BeautifulSoup(self.current_html, "html.parser")
+            a_tags = soup.find_all("a")
+
+            channel = client.get_channel(config.schedule_channel_id)
+            # Clear the channel
+            await channel.purge(limit=10)
+
+            # Send the schedule
+            for a_tag in a_tags:
+                if a_tag.get('href') and '.pdf' in a_tag.get('href'):
+                    await channel.send(f'@everyone {"".join(a_tag.contents).replace("[", "").replace("]", "")}\n{a_tag.get("href")}')
+
+            # Wait 3 days
+            await asyncio.sleep(259200)
 
 
 class my_client(discord.Client):
@@ -67,6 +106,10 @@ class my_client(discord.Client):
 
     async def on_ready(self):
         print('Logged on as', self.user)
+
+        ### Practice schedule ###
+        client.loop.create_task(
+            practice_tracker.get_schedule())
 
     async def on_member_join(self, member):
         ### Welcome ###
@@ -145,6 +188,11 @@ class my_client(discord.Client):
         if message.content.startswith('!help'):
             embed = discord.Embed(
                 title='Commands', description='To see the description of each command use **!help [command]**\n!clear\n!poll\n!filter\n!ai', color=0xFF5733)
+
+            embed.add_field(name='Schedule',
+                            value='Every 3 days the AI scrapes the Uppsala volleyboll official website and gets the schedule and posts it in the schedule discord channel.',
+                            inline=False)
+
             await message.channel.send(embed=embed)
             return
 
@@ -154,25 +202,30 @@ class my_client(discord.Client):
             embed = discord.Embed(
                 title='Clearing...', description='', color=0xFF5733)
             await message.channel.send(embed=embed)
-            await message.channel.purge(limit=10000)
+            # Clear the channel
+            await message.channel.purge(limit=100000)
             return
 
         ### Filter ###
 
+        # Change the filter
         if message.content.startswith('!filter on'):
             filter_content.filter_content = True
             await message.channel.send('Filter now set to True')
             return
 
+        # Change the filter
         if message.content.startswith('!filter off'):
             filter_content.filter_content = False
             await message.channel.send('Filter now set to False')
             return
 
+        # Check the filter
         if message.content.startswith('!filter'):
             await message.channel.send(f'Filter currently set to {filter_content.filter_content}')
             return
 
+        # Filter the message
         if filter_content.filter_content is True and profanity.contains_profanity(message.content) is True:
             await message.delete()
             await message.channel.send(f'{message.author.mention} did a bit of a naughty but I have corrected his ways.\n{profanity.censor(message.content)}')
@@ -181,14 +234,18 @@ class my_client(discord.Client):
         ### Poll ###
 
         if message.content.startswith('!poll'):
+            # Get the message without the command
             msg = message.content.replace('!poll', '')
             embed = discord.Embed(
                 title=f'Poll: {msg}', description='', color=0xFF5733)
             msg = await message.channel.send(embed=embed)
+            # Add initial reactions
             await msg.add_reaction('\N{WHITE HEAVY CHECK MARK}')
             await msg.add_reaction('\N{CROSS MARK}')
 
-            self.poll_finish = client.loop.create_task(self.poll_test(message))
+            # Start the poll countdown for results
+            self.poll_finish = client.loop.create_task(
+                self.poll_result_countdown(message))
             return
 
         ### AI bot ###
@@ -233,15 +290,18 @@ class my_client(discord.Client):
         user_id = payload.user_id
         emoji = payload.emoji
 
+        # Get message
         message = await client.get_channel(channel_id).fetch_message(message_id)
         reaction_users = []
         self.reactions_up = []
         self.reactions_down = []
 
+        # Get all users that reacted to the message
         for reaction in message.reactions:
             async for user in reaction.users():
                 reaction_users.append(user)
 
+        # Remove reactions from users that reacted more than once
         if user_id in [user.id for user in reaction_users] and user_id != self.user.id:
             user_reaction_count = reaction_users.count(
                 client.get_user(user_id))
@@ -249,6 +309,7 @@ class my_client(discord.Client):
             if user_reaction_count >= 2:
                 await message.remove_reaction(emoji, client.get_user(user_id))
 
+            # Get results
             else:
                 if emoji.name == '\N{WHITE HEAVY CHECK MARK}':
                     self.reactions_up.append(message.reactions)
@@ -258,11 +319,14 @@ class my_client(discord.Client):
 
     ### Poll results ###
 
-    async def poll_test(self, message):
+    async def poll_result_countdown(self, message):
         """Poll results"""
 
         while True:
+            # Wait 20 seconds
             await asyncio.sleep(20)
+
+            # Poll results
             embed = discord.Embed(
                 title=f"Poll **{message.content.replace('!poll', '')}** results:",
                 description='',
@@ -283,13 +347,17 @@ class my_client(discord.Client):
 
             await message.channel.send(embed=embed)
 
+            # Cancel the task
             self.poll_finish.cancel()
 
 
+# Define the classes
 filter_content = filter_value()
 ai_bot = ai()
+practice_tracker = practice()
 client = my_client(intents=intents)
 
 
+# Run the bot
 if __name__ == '__main__':
     client.run(config.token)
